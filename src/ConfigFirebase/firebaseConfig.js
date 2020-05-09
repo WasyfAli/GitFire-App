fedcab23fb83aa8fc12a8bfc1d5249f44a3c56bc
WasyfAli <46135061+WasyfAli@users.noreply.github.com>
2020-05-09 18:20:49 +0530

    update firabse config file

--- src/ConfigFirebase/firebaseConfig.js
+++ src/ConfigFirebase/firebaseConfig.js
@@ -1,10 +1,10 @@
 export default  {
-    apiKey: "AIzaSyA8GoRYDQ6lMOVk-oE-MGtcqHy9lz8OvEc",
-    authDomain: "mygitapp-8d4d2.firebaseapp.com",
-    databaseURL: "https://mygitapp-8d4d2.firebaseio.com",
-    projectId: "mygitapp-8d4d2",
-    storageBucket: "mygitapp-8d4d2.appspot.com",
-    messagingSenderId: "59569417471",
-    appId: "1:59569417471:web:bf50daa06ae3f54ab570ca",
-    measurementId: "G-44W6XNECSB"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    databaseURL: "DB_URL",
+    projectId: "ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "MSG_SEND_ID",
+    appId: "APP_ID",
+    measurementId: "MSR_ID"
   };
