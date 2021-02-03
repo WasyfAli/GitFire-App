165bdaae6df899b449e16e87868a84b65889c4a1
root <root@server.example.com>
2021-02-03 04:56:22 -0500

    config

---
+++ src/ConfigFirebase/firebaseConfigExample.js
@@ -0,0 +1,10 @@
+export default  {
+  apiKey: "AIzaSyAqDi8Ba0Y5zBQiAgqGESKyYp3g-7VPN0k",
+    authDomain: "github-api-react.firebaseapp.com",
+    databaseURL: "https://github-api-react.firebaseio.com",
+    projectId: "github-api-react",
+    storageBucket: "github-api-react.appspot.com",
+    messagingSenderId: "786970864057",
+    appId: "1:786970864057:web:56c168c57e16f1384c2e68",
+    measurementId: "G-ND209C9MFF"
+};
